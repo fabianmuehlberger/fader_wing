4187a0a823f76542d77a3c0700c3b90f4d354cbd
fabianmuehlberger <56475821+fabianmuehlberger@users.noreply.github.com>
2022-07-13 11:54:24 +0200

    motor tests

--- code/src/motor_tests.py
+++ code/src/motor_tests.py
@@ -1,13 +1,29 @@
-'''Module is uesed for evaluation purposes. '''
-import motor_control
+'''Module is uesed for evaluation purposes and can be used as a stand alone script  '''
+
+import time
+import utime
+from motor_control import MotorFader
+from machine import Pin, PWM, ADC
+
+if __name__ == " __main__":
+
+    PWM_FREQUENCY = 65000
+
+    enable = PWM(Pin(19))
+    enable.freq(PWM_FREQUENCY)
+    pin12 = Pin(12, Pin.OUT)
+    pin13 = Pin(13, Pin.OUT)
+    adcPin = ADC(Pin(28))
+
+    fader = MotorFader(pin12, pin13, enable, adcPin)
 
 
-'''Test the total movement duration from one side to the other. Duty Cycle used is 62000, 9V supply voltage'''
-def evaluate_movement_duration(duty)
+def evaluate_movement_duration(duty):
+    '''Test the total movement duration from one side to the other. Duty Cycle used is 62000, 9V supply voltage'''
     print("initial Position =", adcPin.read_u16())
 
     time.sleep(1)
-    while adcPin.read_u16() < 65000:
+    while adcPin.read_u16() < PWM_FREQUENCY:
         fader.move_forward(duty)
     fader.stop()
     print("Moved to first position ", adcPin.read_u16())
